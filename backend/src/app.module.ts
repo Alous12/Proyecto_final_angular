61aa4e5c97b9a1da1abdcae996b1531f67392f59
Alous12 <alejandro.machaca@ucb.edu.bo>
2025-06-16 14:10:57 -0400

    Segundo

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { CharactersModule } from './characters/characters.module';
+import { SupabaseModule } from './supabase/supabase.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [CharactersModule, SupabaseModule, ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+  })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
