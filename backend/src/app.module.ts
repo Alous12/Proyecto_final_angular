513f954d2bd7b18e136cf26cf6aeda515a9b0d77
lucianaaaaaaa <luciana.leano@ucb.edu.bo>
2025-06-16 14:00:08 -0400

    primer cambio

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EpisodesModule } from './episodes/episodes.module';
+import { LocationsModule } from './locations/locations.module';
 
 @Module({
-  imports: [],
+  imports: [EpisodesModule, LocationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
