c0273308bfa577cc5f1671a30db57c501cdfe677
lucianaaaaaaa <luciana.leano@ucb.edu.bo>
2025-06-16 14:16:49 -0400

    segundo cambio

--- backend/src/locations/locations.service.ts
+++ backend/src/locations/locations.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { SupabaseService } from 'src/supabase/supabase.service';
 
 @Injectable()
-export class LocationsService {}
+export class LocationsService {
+    constructor(private readonly supabaseService: SupabaseService) {}
+    async findAll() {
+        const supabase = this.supabaseService.getClient();
+        const { data, error } = await supabase
+            .from('locations')
+            .select('*');
+
+        if (error) {
+            throw new Error(`Error fetching locations: ${error.message}`);
+        }
+
+        return data;
+    }
+}
