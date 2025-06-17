1de0d4ad56b07bd5ab2f7295247749cc6d015e97
lucianaaaaaaa <luciana.leano@ucb.edu.bo>
2025-06-16 21:25:54 -0400

    locations

--- backend/src/locations/locations.service.ts
+++ backend/src/locations/locations.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Location } from './location.entity';
 
 @Injectable()
-export class LocationsService {}
+export class LocationsService {
+constructor(
+    @InjectRepository(Location)
+    private readonly locationRepo: Repository<Location>,
+  ) {}
+
+  findAll(): Promise<Location[]> {
+    return this.locationRepo.find();
+  }  
+}
