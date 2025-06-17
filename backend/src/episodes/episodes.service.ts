1852575205521fb5b8ddd62502ef6f861c49de73
Alous12 <alejandro.machaca@ucb.edu.bo>
2025-06-17 16:27:51 -0400

    Relaciones

--- backend/src/episodes/episodes.service.ts
+++ backend/src/episodes/episodes.service.ts
@@ -1,14 +1,18 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { Episode } from './episode.entity';
+import { Character } from 'src/characters/character.entity';
 
 @Injectable()
 export class EpisodesService {
     constructor(
-    @InjectRepository(Episode)
-    private readonly episodeRepo: Repository<Episode>,
-  ) {}
+  @InjectRepository(Episode)
+  private readonly episodeRepo: Repository<Episode>,
+
+  @InjectRepository(Character)
+  private readonly characterRepo: Repository<Character>,
+) {}
 
   findAll(): Promise<Episode[]> {
     return this.episodeRepo.find();
@@ -18,10 +22,19 @@ export class EpisodesService {
     return this.episodeRepo.findOneBy({ id });
   }
 
-    create(episode: Partial<Episode>): Promise<Episode> {
-        const newEpisode = this.episodeRepo.create(episode);
-        return this.episodeRepo.save(newEpisode);
-    }
+    async create(data: Partial<Episode>): Promise<Episode> {
+    const characters = Array.isArray(data.characters)
+      ? await this.characterRepo.findBy({ id: In(data.characters.map(Number)) })
+      : [];
+
+    const episode = this.episodeRepo.create({
+    ...data,
+    characters,
+    });
+
+    return this.episodeRepo.save(episode);
+  }
+
 
     async update(id: number, data: Partial<Episode>): Promise<Episode> {
         const entity = await this.episodeRepo.preload({ id, ...data });
